fdec7354cb2ef40772169540fb55e38f83f0353d
Ivan Kraev <ivankraev2@abv.bg>
2022-07-14 01:43:47 +0300

    update

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,21 +1,31 @@
 import 'bootstrap/dist/css/bootstrap.css'
 import buildClient from '../api/build-client'
+import Header from '../components/Header'
 
+const AppComponent = ({ Component, pageProps, currentUser }) => {
+  
+    return (
+        <div>
+            <Header currentUser={currentUser} />
+            <Component {...pageProps} />
 
-const AppComponent = ({ Component, pageProps }) => {
-    return <Component {...pageProps} />
+        </div>
+    )
 }
 
 AppComponent.getInitialProps = async appContext => {
-    console.log(appContext);
+
     const client = buildClient(appContext.ctx)
-    const { data } = await client.get('/api/users/currentuser')
+    const { data: { currentUser } } = await client.get('/api/users/currentuser')
     let pageProps = {}
     if (appContext.Component.getInitialProps) {
         pageProps = await appContext.Component.getInitialProps(appContext.ctx)
     }
 
-    return data
+    return {
+        currentUser,
+        pageProps
+    }
 }
 
 export default AppComponent
