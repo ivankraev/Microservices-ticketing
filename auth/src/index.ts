ecac1dc964cc511623a95a6e744412f2b7263fd3
Ivan Kraev <ivankraev2@abv.bg>
2022-08-02 02:01:43 +0300

    test

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,6 +5,8 @@ import { app } from "./app";
 const port = process.env.PORT || 3000;
 
 const start = async () => {
+  console.log("starting up...");
+
   try {
     await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
     console.log("Connected to db");
